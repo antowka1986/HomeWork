58307d066ed15765f6ab0b69c618dc12cc36ee3b
antowka1986 <kvasnitskiyanton42@gmail.com>
2021-11-21 17:04:36 +0200

    added homework 7 and 8

---
+++ while.py
@@ -0,0 +1,12 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Введите значение числа А=")
+A = int(input())
+print("Введите значение числа В=")
+B = int(input())
+if A > B:
+    print("Значение А больше значения B")
+elif A < B:
+    print("Второе значение то больше и от этого - So sad, Ешкин кот")
+else:
+    print("АхРеНеТь, а значения то равны")
